697440b1a0d1d5b001df19bbd6be9cdf8217c5c9
Sampush <68147813+sampush@users.noreply.github.com>
2023-07-31 16:07:03 +0100

    New updates

--- src/Component/Side/Side.js
+++ src/Component/Side/Side.js
@@ -1,27 +1,153 @@
 import React from "react";
 import "./Side.css";
 import small from "../imago/small3.png";
+import small2 from "../../Component/lommy2.png";
+import small3 from "../../Component/lommy3.png";
 
 function Side() {
   return (
     <div>
-      <div className="small-top">
-        <div>
-          <img src={small} alt="" />
-        </div>
-        <div className="small-top2">
-          <div className="">
-            <div className="small-border">
-              <h6>BIKIN DEAL</h6>
-              <p>Size: Small</p>
-              <p>Color: Till</p>
+      <div className="small-bop">
+        <img src={small} alt="" />
+
+        <div className="small-top">
+          <div className="small-top2">
+            <div>
+              <h6 className="smart">BIKIN DEAL</h6>
+              <p className="dump">Size: Small</p>
+              <p className="dump">Color: Till</p>
+            </div>
+
+            <div>
+              <h6 className="h6-d">Remove</h6>
             </div>
           </div>
-          <div>
-            <h6>Remove</h6>
+
+          <div className="small-border">
+            <div className="small-i">
+              <p>Qty</p>
+            </div>
+            <div>
+              <h6>$68.1</h6>
+            </div>
           </div>
         </div>
       </div>
+
+      <div className="small-bop">
+        <img src={small3} alt="" />
+
+        <div className="small-top">
+          <div className="small-top2">
+            <div>
+              <h6 className="smart">BIKIN DEAL</h6>
+              <p className="dump">Size: Small</p>
+              <p className="dump">Color: Till</p>
+            </div>
+
+            <div>
+              <h6 className="h6-d">Remove</h6>
+            </div>
+          </div>
+
+          <div className="small-border">
+            <div className="small-i">
+              <p>Qty</p>
+            </div>
+            <div>
+              <h6>$68.1</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="small-bop">
+        <img src={small2} alt="" />
+
+        <div className="small-top">
+          <div className="small-top2">
+            <div>
+              <h6 className="smart">BIKIN DEAL</h6>
+              <p className="dump">Size: Small</p>
+              <p className="dump">Color: Till</p>
+            </div>
+
+            <div>
+              <h6 className="h6-d">Remove</h6>
+            </div>
+          </div>
+
+          <div className="small-border">
+            <div className="small-i">
+              <p>Qty</p>
+            </div>
+            <div>
+              <h6>$68.1</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+
+        {/* {2} */}
+      <div className="dis-bord">
+        <div>
+          <h5>DISCOUNT CODE</h5>
+        </div>
+
+        <div className="input-flex">
+          <div className="input-d">
+              <input text="discount" />
+          </div>
+
+          <div className="input-box">
+            <h6>Apply</h6>
+          </div>
+        </div>
+      </div>
+        
+        {/* {3} */}
+
+      <div className="sum-bord">
+        <div className="sum-pad">
+          <h2>ORDER SUMMARY</h2>
+        </div>
+
+        <div>
+          <div className="total-flex">
+            
+              <p>Subtotal</p>
+            
+            <div>
+              <p>$204.03 USD</p>
+            </div>
+          </div>
+
+          <div className="total-flex">
+            
+              <p>Flat rate</p>
+            
+            <div>
+              <p>$18.93 USD</p>
+            </div>
+          </div>
+
+          <div className="total-flex">
+              <div className="move-total">
+                <p>Total</p>
+              </div>
+              
+            
+            <div>
+              <p>$229.23 USD</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="input-box">
+            <h6>Pay and Place order- $ 229.23 USD</h6>
+          </div>
+        
+      </div> 
     </div>
   );
 }
